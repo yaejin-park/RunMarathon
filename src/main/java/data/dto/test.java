a4af8b51bed9e81cd35fbb5e51295ac0338a9a3d
minjee <kimmj1106@naver.com>
2021-10-07 10:47:40 +0900

    1007 ProjectTest

---
+++ src/main/java/data/dto/test.java
@@ -0,0 +1,11 @@
+package data.dto;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World2");
+		System.out.println("건들지 마세요. 테스트 파일입니다.2");
+	}
+
+}
---
+++ src/main/java/mysql/db/test.java
@@ -0,0 +1,11 @@
+package mysql.db;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World3");
+		System.out.println("건들지 마세요. 테스트 파일입니다.3");
+	}
+
+}
