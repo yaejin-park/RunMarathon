54774c53b7d5eaef0a27e795702d7624d2ad1c60
HANHEEGUE <63626124+HANHEEGUE@users.noreply.github.com>
2021-10-08 15:38:50 +0900

    hhk_menu upload
    메뉴 db 연결, 메뉴 기능, 서브페이지 왼쪽메뉴 기능 구현

--- src/main/webapp/common/js/script.js
+++ src/main/webapp/common/js/script.js
@@ -1,11 +1,34 @@
 $(function(){
+	$("header .menu-btn").mouseenter(function(){
+		if(!$(this).hasClass("active")){
+			gsap.to($(this).find("span").eq(0),0.4, {width:0, yoyo:true, repeat:1, ease:Power3.easeOut});	
+			gsap.to($(this).find("span").eq(1),0.4, {delay:0.1, width:0, yoyo:true, repeat:1, ease:Power3.easeOut});	
+			gsap.to($(this).find("span").eq(2),0.4, {delay:0.2, width:0, yoyo:true, repeat:1, ease:Power3.easeOut});
+		}	
+	});
+	
+	$("header .menu-btn").mouseleave(function(){
+		if(!$(this).hasClass("active")){
+			gsap.to($(this).find("span").eq(0),0.4, {width:"100%", ease:Power3.easeOut});	
+			gsap.to($(this).find("span").eq(1),0.4, {delay:0, width:"100%", ease:Power3.easeOut});
+			gsap.to($(this).find("span").eq(2),0.4, {delay:0, width:"100%", ease:Power3.easeOut});
+		}
+	});
+	
 	$("header .menu-btn").click(function(){
 		if(!$(this).hasClass("active")){
-			$(this).addClass("active");
+			gsap.to($(this).find("span").eq(0), 0.4,{top:14, rotate:"45deg", ease:Power3.easeOut});
+			gsap.to($(this).find("span").eq(1), 0.4,{opacity:0, ease:Power3.easeOut});
+			gsap.to($(this).find("span").eq(2), 0.4,{top:14, rotate:"-45deg", ease:Power3.easeOut, onComplete:function(){
+				$("header .menu-btn").addClass("active");
+			}});
 			$("header").addClass("active");
 			$("header .menu").fadeIn(500);	
 		}else{
 			$(this).removeClass("active");
+			gsap.to($(this).find("span").eq(0), 0.4,{top:"", rotate:0, ease:Power3.easeOut});
+			gsap.to($(this).find("span").eq(1), 0.4,{opacity:1, ease:Power3.easeOut});
+			gsap.to($(this).find("span").eq(2), 0.4,{top:"", rotate:0, ease:Power3.easeOut});
 			$("header").removeClass("active");
 			$("header .menu").fadeOut(500);	
 		}
---
+++ src/main/webapp/info/course.jsp
@@ -0,0 +1,3 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+코스페이지입니다.
