e6004738853de8d03d3d5a41091b174cb23b8991
YAEJINPARK <pyjin805@naver.com>
2021-10-15 14:10:40 +0900

    pyj_ commit test(macbook)

--- src/main/java/mysql/db/DBConnect.java
+++ src/main/java/mysql/db/DBConnect.java
@@ -15,7 +15,6 @@ public class DBConnect {
 	public DBConnect() {
 		try {
 			Class.forName(MYSQLDRIVER);
-			System.out.println("aws Mysql success");
 		} catch (ClassNotFoundException e) {
 			System.out.println("aws Mysql fail:"+e.getMessage());
 		}
@@ -25,7 +24,6 @@ public class DBConnect {
 		Connection conn = null;
 		try {
 			conn = DriverManager.getConnection(MYSQL_URL, "admin", "asdf7890");
-			System.out.println("aws Mysql getConnection success");
 		} catch (SQLException e) {
 			System.out.println("Mysql fail :"+e.getMessage());
 		}
